59570e887859111b7523a4144b3a5c689ea08c9e
tran hoang ngoc dat <datdat13055@gmail.com>
2026-04-15 15:17:12 +0700

    video 20

--- src/components/Mycomponent.js
+++ src/components/Mycomponent.js
@@ -3,12 +3,26 @@
 
 import React  from "react";
 class Mycomponent extends React.Component{
-   handleClick(event){
-    console.log(">>click me my button")
+   handleClick=(event)=>{
+    console.log(">>click me my button");
+    console.log("my name is",this.state.name);
+// cap nhat state cua component
+    this.setState({
+        name:'datdepzai',
+        age:Math.floor((Math.random() * 100) + 1)
+    })
+     this.setState({
+        age:Math.floor((Math.random() * 100) + 1)
+    })
+    
     //console.log(event.target)
    }
-   handleOnMoveOver(event){
-          console.log(event.pageX)
+   handleOnMoveOver = () => {
+        const randomAge = Math.floor((Math.random() * 100) + 1);
+        console.log("random", randomAge);
+        this.setState({
+            age: randomAge
+        });
    }
     //jsx
        state={
@@ -16,11 +30,11 @@ class Mycomponent extends React.Component{
           address:'ha noi',
           age:16        };
          render(){
-          return(
+         return(
               <div>  
-                  my name is { this.state.name}  and i am from { this.state.address}
-                      <button onMouseOver={this.handleClick }>hover me</button>  
-                        <button onClick={this.handleClick }>Click me</button>  
+                  my name is { this.state.name}  and i am { this.state.age}
+                      <button onMouseOver={this.handleOnMoveOver}>hover me</button>  
+                        <button onClick={(event)=> {this.handleClick(event)} }>Click me</button>  
                 
                   </div>
           );
