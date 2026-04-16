2513649e685db36933f7a680a77d0a1112135268
tran hoang ngoc dat <datdat13055@gmail.com>
2026-04-16 23:27:16 +0700

    video 34

--- src/components/Displayinfor.js
+++ src/components/Displayinfor.js
@@ -1,63 +1,64 @@
 import React from "react";
 import './Displayinfor.scss'
 import logo from './../logo.svg';
-class Displayinfor extends React.Component{
-   state={
-       isShowlistUser:true
-   }
-     constructor(props){
-        console.log("call me constructor")  
-        super(props);
-        this.state={
-            isShowlistUser:true
-        }
-     }
-     componentDidMount(){
-        console.log("call me component did mount")
-        setTimeout(() => {
-            document.title="hello dat"
-        }, 3000);
-     }
-     componentDidUpdate(prevProps,prevState,snapshot){
-        console.log("call me component did update",this.props,prevProps)
-        if(this.props.listUser !== prevProps.listUser){
-                if(this.props.listUser.length === 5){
-                    alert("you have 5 user in your list")
-                }
-            }
-        }
-
-
-    handleShowhide=()=>{
-            this.setState((prevState)=>({
-                isShowlistUser:!prevState.isShowlistUser
-            }))
-
-    }
+// class Displayinfor extends React.Component{
+  
     
-    render(){
-        console.log("call me render")
-        const {listUser}=this.props;
+//     render(){
+//         console.log("call me render")
+//         const {listUser}=this.props;
         
-       // console.log(this.props)
-       //dry 
-       console.table(listUser)
+//        // console.log(this.props)
+//        //dry 
+//        console.table(listUser)
+//         return(
+//             <div className="display-infor-container">
+            
+//                 {true&& 
+//             <>
+//                 {listUser.map((user)=>{
+//                      return(
+//                         <div key={user.id} className={+user.age>18?"green":"red"} >
+//                               <div >my name is {user.name}</div>
+//                              <div>my age {user.age} </div>
+//                              <button onClick={()=>this.props.handleDeleteUser(user.id)}>Delete</button>
+//                         </div>
+//                     )
+
+
+                  
+                   
+//                 })}
+//                 {/* <div>my name is {name}</div>
+//                 <div>my age {age}</div>
+//                 <hr></hr>
+//                  <div>my name is {name}</div>
+//                 <div>my age {age}</div>
+//                 <hr></hr>
+//                  <div>my name is {name}</div>
+//                 <div>my age {age}</div> */}
+//             </>
+//                  }
+
+
+//             </div>
+//         )
+//     }
+// }
+const Displayinfor=(props)=>{
+     const {listUser}=props;
+  
         return(
             <div className="display-infor-container">
-                {/* <img src={logo}/> */}
-                <div>
-                    <span onClick={()=>{this.handleShowhide()}}>
-                        {this.state.isShowlistUser=== true? "hide list user:":"show list user" }
-                    </span>
-                </div>
-                {this.state.isShowlistUser && 
+            
+                {true&& 
             <>
                 {listUser.map((user)=>{
                      return(
                         <div key={user.id} className={+user.age>18?"green":"red"} >
                               <div >my name is {user.name}</div>
                              <div>my age {user.age} </div>
-                             <button onClick={()=>this.props.handleDeleteUser(user.id)}>Delete</button>
+                             <button onClick={()=>props.handleDeleteUser(user.id)}>Delete</button>
                         </div>
                     )
 
@@ -80,5 +81,6 @@ class Displayinfor extends React.Component{
             </div>
         )
     }
-}
+
+
 export default Displayinfor;
