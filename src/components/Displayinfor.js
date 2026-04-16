739ceb917c9ee05c9f4040debc6114d4d1122f8a
tran hoang ngoc dat <datdat13055@gmail.com>
2026-04-16 09:25:25 +0700

    video25

--- src/components/Displayinfor.js
+++ src/components/Displayinfor.js
@@ -1,20 +1,40 @@
 import React from "react";
 class Displayinfor extends React.Component{
+   state={
+       isShowlistUser:true
+   }
+
+    handleShowhide=()=>{
+            this.setState({isShowlistUser:!this.state.isShowlistUser})
+
+    }
     
     render(){
         const {listUser}=this.props;
         
        // console.log(this.props)
        //dry 
+       console.table(listUser)
         return(
             <div>
+                <div>
+                    <span onClick={()=>{this.handleShowhide()}}>
+                        {this.state.isShowlistUser=== true? "hide list user:":"show list user" }
+                    </span>
+                </div>
+                {this.state.isShowlistUser && 
+            <div>
                 {listUser.map((user)=>{
-                    return(
-                        <div key={user.id}>
+                     return(
+                        <div key={user.id} className={+user.age>18?"green":"red"} >
                               <div>my name is {user.name}</div>
                              <div>my age {user.age} </div>
                         </div>
                     )
+
+
+                  
+                   
                 })}
                 {/* <div>my name is {name}</div>
                 <div>my age {age}</div>
@@ -24,6 +44,10 @@ class Displayinfor extends React.Component{
                 <hr></hr>
                  <div>my name is {name}</div>
                 <div>my age {age}</div> */}
+            </div>
+                 }
+
+
             </div>
         )
     }
